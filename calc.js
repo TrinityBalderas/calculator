26f7448e146e45482c312f4fc5aa6c55def87850
Trinity Balderas <trinitybalderas02@gmail.com>
2022-07-12 23:01:13 -0500

    adding calculator

---
+++ calc.js
@@ -0,0 +1,50 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	const tokens = input.split(' ');
+	
+	const mathSymbol = tokens[0];
+	const num1 = Number(tokens[1]);
+	const num2 = Number(tokens[2]);
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+	else if(mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+	else if(mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+	else if(mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+	else if(mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
+	}
+	else if(mathSymbol === "**"){
+		if(num2 === "2"){
+			console.log(num1 ** 2);
+		}
+		else if(num2 === "3"){
+			console.log(num1 ** 3);
+		}
+		else{ 
+			console.log(num1 ** num2);
+		}
+	}
+	else if(mathSymbol === "%"){
+		console.log(num1 % num2);
+	}
+	
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
